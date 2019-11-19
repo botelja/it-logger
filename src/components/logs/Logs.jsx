e20b1369058c2f4ca28cfc0822aba3f71af53880
botelja <ivo.surjak@gmail.com>
2019-11-19 21:08:16 +0100

    Add LogItem and Preloader component

--- src/components/logs/Logs.jsx
+++ src/components/logs/Logs.jsx
@@ -1,35 +1,39 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import LogsItem from './LogsItem';
+import Preloader from '../layout/Preloader';
 
 const Logs = () => {
-    const [logs, setLogs] = useState([])
-    const [loading, setLoading] = useState(false)
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        getLogs()
-    }, [])
+  useEffect(() => {
+    getLogs();
+  }, []);
 
-    const getLogs = async () => {
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch('/logs');
+    const data = await res.json();
 
-        setLoading(true)
-        const res = await fetch('/logs');
-        const data = await res.json();
+    setLogs(data);
+    setLoading(false);
+  };
 
-        setLogs(data);
-        setLoading(false)
+  if (loading) {
+    return <Preloader />;
+  }
+  return (
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <h4 className="center">System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className="center">No logs to show...</p>
+      ) : (
+        logs.map((log) => <LogsItem key={log.id} log={log} />)
+      )}
+    </ul>
+  );
+};
 
-    }
-
-    if(loading) {
-        return <h4>Loading...</h4>
-    }
-    return (
-        <ul className="collection with-header">
-            <li className="collection-header">
-                <h4 className="center">System Logs</h4>
-            </li>
-            {!loading && logs.length === 0 ? (<p className="center">No logs to show...</p>) : (logs.map(log => <li className="collection-item">{log.message}</li>))}
-        </ul>
-    )
-}
-
-export default Logs
+export default Logs;
